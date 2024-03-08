5a82c96d435d4f12252e35d9c944df349aa55650
Beasleydog <beasleypero@gmail.com>
2024-03-08 16:31:05 -0500

    Refactor code for Schoology assignment magic button

--- schoologyAssignment.js
+++ schoologyAssignment.js
@@ -1,7 +1,8 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     //Only run on Schoology discussions
-    if(window!=window.top)return;
-   
+    if (window != window.top) return;
+
+    const DEFAULT_MAGIC_TEXT = "Magic 🪄";
     let PARENT = document.getElementsByClassName("right-block-big")[0];
     let MAGIC_BUTTON = document.createElement("div");
     MAGIC_BUTTON.classList.add("link-btn");
@@ -9,14 +10,14 @@ window.addEventListener("load",()=>{
     MAGIC_BUTTON.classList.add("dropbox-submit");
     MAGIC_BUTTON.classList.add("popups-processed");
     MAGIC_BUTTON.classList.add("sExtlink-processed");
-    MAGIC_BUTTON.style.marginTop="4px";
-    MAGIC_BUTTON.innerText="Magic 🪄";
-    MAGIC_BUTTON.onclick=doTheMagic;
+    MAGIC_BUTTON.style.marginTop = "4px";
+    MAGIC_BUTTON.innerText = DEFAULT_MAGIC_TEXT;
+    MAGIC_BUTTON.onclick = doTheMagic;
     PARENT.appendChild(MAGIC_BUTTON);
 
     async function askAI(text, streamHook) {
         const MAX_TOKENS = 1000;
-        const SYTEM_PROMPT = "Make sure to use \n to signify a line break in your response.";
+        const SYTEM_PROMPT = "Make sure to use \n to signify a line break in your response. Keep all sentences in your response short. Use mostly sentence fragments, don't focus too much on proper sentences.";
 
         text = SYTEM_PROMPT + text;
 
@@ -48,20 +49,20 @@ window.addEventListener("load",()=>{
             let indexsAdded = [];
             while (true) {
                 const { done, value } = await reader.read();
-                
-                
+
+
                 let newData = new TextDecoder("utf-8").decode(value);
                 console.log(newData);
                 newData.split("}\n\n").forEach((chunk) => {
                     chunk = chunk.replace("data:", "");
-                    if(chunk.charAt(chunk.length-1)!="}"){
-                        chunk=chunk+"}";
+                    if (chunk.charAt(chunk.length - 1) != "}") {
+                        chunk = chunk + "}";
                     }
                     try {
                         chunk = JSON.parse(chunk);
                         if (!indexsAdded.includes(chunk.index)) {
                             indexsAdded.push(chunk.index);
-                            streamHook(chunk.token.text,chunk.generated_text);
+                            streamHook(chunk.token.text, chunk.generated_text);
                         }
                     } catch { };
                 });
@@ -71,23 +72,51 @@ window.addEventListener("load",()=>{
             }
         }
     }
-    async function createDoc(title,content){
-        const APPSCRIPT_URL="https://script.google.com/macros/s/AKfycbwBjAiL4xyomwuzfl06bCvcRnzCC8nQ_eA9M_K48-DFajyLBWFTRp-r3XPuDsc18Sbq/exec";
+    async function createDoc(title, content) {
+        const APPSCRIPT_URL = "https://script.google.com/macros/s/AKfycbwBjAiL4xyomwuzfl06bCvcRnzCC8nQ_eA9M_K48-DFajyLBWFTRp-r3XPuDsc18Sbq/exec";
         let url = await fetch(`${APPSCRIPT_URL}?title=${title}&content=${content}`);
         url = await url.text();
         return url;
     }
-    async function doTheMagic(){
+    async function doTheMagic() {
         const QUESTION_TEXT = document.getElementsByClassName("info-container")[1].innerText;
-        let docYet=false;
-        askAI(QUESTION_TEXT,async (t,fullText)=>{
-            console.log(t,fullText);
-            if(fullText&&!docYet){
-                docYet=true;
+        let docYet = false;
+        let returned = 0;
+        askAI(QUESTION_TEXT, async (t, fullText) => {
+            returned++;
+            MAGIC_BUTTON.innerText = returned;
+            console.log(t, fullText);
+            if (fullText && !docYet) {
+
+                //Have to preserve the line breaks because they get lost in the url otherwise
+                fullText = fullText.replaceAll("\n", "NLCGRH");
+
+                docYet = true;
                 const QUESTION_TITLE = document.getElementsByClassName("page-title")[0].innerText;
                 const NAME = document.querySelectorAll(`div[data-sgy-sitenav="header-my-account-menu"]`)[0].children[0].children[0].children[0].children[0].alt;
-                const DOC_NAME = `${NAME} - ${QUESTION_TITLE}`; 
-                console.log(await createDoc(DOC_NAME,fullText));
+                const DOC_NAME = `${NAME} - ${QUESTION_TITLE}`;
+                const DOC_URL = await createDoc(DOC_NAME, fullText);
+                MAGIC_BUTTON.innerText = DEFAULT_MAGIC_TEXT;
+
+                //Open submit popup
+                document.getElementsByClassName("submit-assignment")[0].children[0].click();
+
+                //Navigate to create tab
+                (await new Promise((res) => {
+                    let int = setInterval(() => {
+                        const CREATE_BUTTON = document.getElementById("dropbox-submit-create-tab");
+                        if (CREATE_BUTTON) {
+                            setTimeout(() => {
+                                res(CREATE_BUTTON);
+                            }, 500);
+                        }
+                    }, 100);
+                })).click();
+
+                //Schoology is kinda slow, give it some time to load
+                setTimeout(() => {
+                    tinyMCE.activeEditor.execCommand('mceInsertContent', false, DOC_URL);
+                }, 500);
             }
         });
     }
